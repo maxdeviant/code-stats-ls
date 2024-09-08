3909d06a524025f573972b2cacddaccf694fb4d2
Marshall Bowers <elliott.codes@gmail.com>
2024-09-08 14:26:00 -0400

    Ensure writes are committed

--- src/cache.rs
+++ src/cache.rs
@@ -1,3 +1,5 @@
+use std::path::Path;
+
 use anyhow::{Context, Result};
 use heed::types::SerdeBincode;
 use heed::{Database, EnvOpenOptions};
@@ -14,6 +16,10 @@ impl PulseCache {
         let data_dir = dirs::data_dir().context("no data directory found")?;
         let database_path = data_dir.join("code-stats-ls/cache");
 
+        Self::from_path(database_path)
+    }
+
+    fn from_path(database_path: impl AsRef<Path>) -> Result<Self> {
         std::fs::create_dir_all(&database_path)
             .context("failed to create cache database directory")?;
 
@@ -46,6 +52,7 @@ impl PulseCache {
     pub fn save(&self, pulse: &Pulse) -> Result<()> {
         let mut tx = self.env.write_txn()?;
         self.database.put(&mut tx, &pulse.coded_at, pulse)?;
+        tx.commit()?;
 
         Ok(())
     }
@@ -53,7 +60,71 @@ impl PulseCache {
     pub fn remove(&self, pulse: &Pulse) -> Result<()> {
         let mut tx = self.env.write_txn()?;
         self.database.delete(&mut tx, &pulse.coded_at)?;
+        tx.commit()?;
 
         Ok(())
+    }
+
+    pub fn clear(&self) -> Result<()> {
+        let mut tx = self.env.write_txn()?;
+        self.database.clear(&mut tx)?;
+        tx.commit()?;
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use chrono::Local;
+
+    use crate::PulseXp;
+
+    use super::*;
+
+    #[test]
+    fn test_pulse_cache() {
+        let pulse_cache = PulseCache::from_path("./test_database").unwrap();
+
+        pulse_cache.clear().unwrap();
+
+        let initial_pulses = pulse_cache.list().unwrap();
+        assert_eq!(initial_pulses.len(), 0);
+
+        let first_pulse = Pulse {
+            coded_at: Local::now().to_rfc3339(),
+            xps: vec![PulseXp {
+                language: "Rust".into(),
+                xp: 10,
+            }],
+        };
+
+        pulse_cache.save(&first_pulse).unwrap();
+
+        let pulses = pulse_cache.list().unwrap();
+        assert_eq!(pulses.len(), 1);
+
+        let second_pulse = Pulse {
+            coded_at: Local::now().to_rfc3339(),
+            xps: vec![PulseXp {
+                language: "Gleam".into(),
+                xp: 20,
+            }],
+        };
+
+        pulse_cache.save(&second_pulse).unwrap();
+
+        let pulses = pulse_cache.list().unwrap();
+        assert_eq!(pulses.len(), 2);
+
+        pulse_cache.remove(&second_pulse).unwrap();
+
+        let pulses = pulse_cache.list().unwrap();
+        assert_eq!(pulses.len(), 1);
+
+        pulse_cache.clear().unwrap();
+
+        let pulses = pulse_cache.list().unwrap();
+        assert_eq!(pulses.len(), 0);
     }
 }
